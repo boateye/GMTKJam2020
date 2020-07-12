458299012ad65becfef16cfc7f1c6a864fe66afe
DESKTOP-733DGO4\Alex <zeac79@gmail.com>
2020-07-12 02:32:32 -0700

    added sprite needs layer fixing and added UI to game scene

---
+++ GMTKJam2020/Assets/Scripts/SpriteAnimator.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteAnimator : MonoBehaviour
+{
+    [SerializeField] private Sprite[] frameArray;
+    private int currentFrame;
+    private float timer;
+    private float framerate = .1f;
+    private SpriteRenderer spriteRenderer;
+
+
+    private void Awake()
+    {
+        {
+            spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+
+        }
+    }
+    private void Update()
+    {
+        timer += Time.deltaTime;
+
+        if (timer >= framerate)
+        {
+            timer -= framerate;
+            currentFrame = (currentFrame +1) % frameArray.Length;
+            spriteRenderer.sprite = frameArray[currentFrame];
+        }
+
+    }
+}
