c89facff2e679fb60d3effdc00e59140798023db
ONYX\jorda <jordan.m.a.johnson@gmail.com>
2020-07-11 22:51:42 -0700

    added lists of messages

--- GMTKJam2020/Assets/Scripts/NamesAndMessages.cs
+++ GMTKJam2020/Assets/Scripts/NamesAndMessages.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class NamesAndMessages : MonoBehaviour
 {
-    ArrayList 
+    ArrayList usernameList = new ArrayList();
+    ArrayList messagesList = new ArrayList();
 
     // Start is called before the first frame update
     void Start()
